dac21a7a32fb2c1846b7acf444f896c3fac4f868
RK03-B1245 <RK03-B1245@RK03-B1245-13>
2018-03-14 15:03:47 -0500

    Add project files.

---
+++ RegistrationPro/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace RegistrationPro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.modeDropDown = new System.Windows.Forms.ComboBox();
+            this.modeLabel = new System.Windows.Forms.Label();
+            this.adminPasswordLabel = new System.Windows.Forms.Label();
+            this.adminPasswordBox = new System.Windows.Forms.TextBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.hideLabelPicBox = new System.Windows.Forms.PictureBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.payLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.hideLabelPicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(170, 19);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(307, 46);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Registration Pro";
+            this.titleLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // modeDropDown
+            // 
+            this.modeDropDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modeDropDown.FormattingEnabled = true;
+            this.modeDropDown.Items.AddRange(new object[] {
+            "Register/Pay",
+            "Administrator Functions",
+            "Sign In"});
+            this.modeDropDown.Location = new System.Drawing.Point(44, 131);
+            this.modeDropDown.Name = "modeDropDown";
+            this.modeDropDown.Size = new System.Drawing.Size(271, 33);
+            this.modeDropDown.TabIndex = 1;
+            this.modeDropDown.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // modeLabel
+            // 
+            this.modeLabel.AutoSize = true;
+            this.modeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modeLabel.Location = new System.Drawing.Point(39, 87);
+            this.modeLabel.Name = "modeLabel";
+            this.modeLabel.Size = new System.Drawing.Size(167, 25);
+            this.modeLabel.TabIndex = 2;
+            this.modeLabel.Text = "Chose your mode";
+            // 
+            // adminPasswordLabel
+            // 
+            this.adminPasswordLabel.AutoSize = true;
+            this.adminPasswordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adminPasswordLabel.Location = new System.Drawing.Point(39, 183);
+            this.adminPasswordLabel.Name = "adminPasswordLabel";
+            this.adminPasswordLabel.Size = new System.Drawing.Size(196, 25);
+            this.adminPasswordLabel.TabIndex = 3;
+            this.adminPasswordLabel.Text = "Enter your password:";
+            this.adminPasswordLabel.Visible = false;
+            // 
+            // adminPasswordBox
+            // 
+            this.adminPasswordBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adminPasswordBox.Location = new System.Drawing.Point(329, 183);
+            this.adminPasswordBox.Name = "adminPasswordBox";
+            this.adminPasswordBox.Size = new System.Drawing.Size(193, 30);
+            this.adminPasswordBox.TabIndex = 4;
+            this.adminPasswordBox.Visible = false;
+            // 
+            // goButton
+            // 
+            this.goButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goButton.Location = new System.Drawing.Point(44, 236);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(101, 40);
+            this.goButton.TabIndex = 5;
+            this.goButton.Text = "Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(860, 393);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage1.Controls.Add(this.goButton);
+            this.tabPage1.Controls.Add(this.adminPasswordBox);
+            this.tabPage1.Controls.Add(this.titleLabel);
+            this.tabPage1.Controls.Add(this.adminPasswordLabel);
+            this.tabPage1.Controls.Add(this.modeDropDown);
+            this.tabPage1.Controls.Add(this.modeLabel);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(689, 364);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.comboBox1);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.payLabel);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(852, 364);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // hideLabelPicBox
+            // 
+            this.hideLabelPicBox.BackColor = System.Drawing.Color.White;
+            this.hideLabelPicBox.Location = new System.Drawing.Point(12, 506);
+            this.hideLabelPicBox.Name = "hideLabelPicBox";
+            this.hideLabelPicBox.Size = new System.Drawing.Size(689, 26);
+            this.hideLabelPicBox.TabIndex = 6;
+            this.hideLabelPicBox.TabStop = false;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(689, 334);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(689, 334);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // payLabel
+            // 
+            this.payLabel.AutoSize = true;
+            this.payLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.payLabel.Location = new System.Drawing.Point(193, 17);
+            this.payLabel.Name = "payLabel";
+            this.payLabel.Size = new System.Drawing.Size(259, 46);
+            this.payLabel.TabIndex = 0;
+            this.payLabel.Text = "Register/ Pay";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Credit Card Number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(47, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(191, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Name of Competitor:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(47, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Select Competition(s):";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(296, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(530, 30);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(296, 130);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(530, 30);
+            this.textBox2.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "3D Animation- HS",
+            "Animatronics- HS",
+            "Architectural Design- HS",
+            "Biotechnology Design- HS",
+            "Chapter Team- HS",
+            "Children\'s Stories- HS",
+            "Coding- HS",
+            "Computer-Aided Design (CAD), Architechture- HS",
+            "Computer-Aided Design (CAD), Engineering- HS",
+            "Computer Integrated Manufacturing (CIM)- HS",
+            "Debating Technological Issues- HS",
+            "Digital Video Production- HS",
+            "Dragster Design- HS",
+            "Engineering Design- HS",
+            "Essays on Technology- HS",
+            "Extemporaneous Speech- HS",
+            "Fashion Design and Technology- HS",
+            "Flight Endurance- HS",
+            "Future Technology Teacher- HS",
+            "Music Production- HS",
+            "On Demand Video- HS",
+            "Photographic Technology- HS",
+            "Prepared Presentation- HS",
+            "Promotional Design- HS",
+            "Scientific Visualization (SciVis)- HS",
+            "Software Development- HS",
+            "STEM Careers- HS",
+            "Structural Design and Engineering- HS",
+            "System Control Technology- HS",
+            "Technology Bowl- HS",
+            "Technology Problem Solving- HS",
+            "Transportation Modeling- HS",
+            "Video Game Design- HS",
+            "Webmaster- HS"});
+            this.comboBox1.Location = new System.Drawing.Point(296, 169);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(530, 33);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 557);
+            this.Controls.Add(this.hideLabelPicBox);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.hideLabelPicBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.ComboBox modeDropDown;
+        private System.Windows.Forms.Label modeLabel;
+        private System.Windows.Forms.Label adminPasswordLabel;
+        private System.Windows.Forms.TextBox adminPasswordBox;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox hideLabelPicBox;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label payLabel;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ RegistrationPro/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistrationPro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            modeDropDown.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (modeDropDown.SelectedIndex == 1)
+            {
+                adminPasswordBox.Visible = true;
+                adminPasswordLabel.Visible = true;
+            }
+            else
+            {
+                adminPasswordBox.Visible = false;
+                adminPasswordLabel.Visible = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
